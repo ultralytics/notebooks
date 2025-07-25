b9df0bfc97754d7a80d682d1639b3c424239cc77
UltralyticsAssistant <web@ultralytics.com>
2025-07-25 06:05:15 +0000

    Auto-format by https://ultralytics.com/actions

--- docs/update-readme-table.py
+++ docs/update-readme-table.py
@@ -30,9 +30,7 @@ for nb in data["notebooks"]:
     # Ultralytics blog
     blog_badge = ""
     if nb.get("blog"):
-        blog_badge = (
-            f'<a href="{nb["blog"]}"><img src="https://github.com/user-attachments/assets/c60c360b-69de-4228-8545-f83096d5a9ce" alt="YouTube"></a>'
-        )
+        blog_badge = f'<a href="{nb["blog"]}"><img src="https://github.com/user-attachments/assets/c60c360b-69de-4228-8545-f83096d5a9ce" alt="YouTube"></a>'
     # YouTube badge with custom image
     youtube_badge = ""
     if nb.get("youtube"):
