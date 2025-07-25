ae86121465844a5b6b5a051556fc9f25a6b427df
Muhammad Rizwan Munawar <muhammadrizwanmunawar123@gmail.com>
2025-07-25 10:45:07 +0500

    update

--- docs/update-readme-table.py
+++ docs/update-readme-table.py
@@ -1,8 +1,8 @@
 # Ultralytics 🚀 AGPL-3.0 License - https://ultralytics.com/license
 
-# This Python script is responsible for programmatically updating the notebooks
-# table within the README.md file. It ensures that any newly added or modified
-# notebooks are automatically reflected in the documentation.
+# Programmatically updating the notebooks table within the README.md file.
+# It ensures that any newly added or modified notebooks are automatically
+# reflected in the documentation.
 
 import yaml
 
