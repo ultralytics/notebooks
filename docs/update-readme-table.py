964ff3a9dd7d836845016fa1115780b85246188d
Muhammad Rizwan Munawar <muhammadrizwanmunawar123@gmail.com>
2025-07-25 10:38:01 +0500

    update

--- docs/update-readme-table.py
+++ docs/update-readme-table.py
@@ -7,7 +7,7 @@
 
 import yaml
 
-with open("notebooks-data.yml") as f:  # Load notebooks from YAML
+with open("notebooks.yml") as f:  # Load notebooks from YAML
     data = yaml.safe_load(f)
 
 # Generate table
