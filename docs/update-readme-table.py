58716c90055afb300dcf6cf0660f5441f04ce202
Muhammad Rizwan Munawar <muhammadrizwanmunawar123@gmail.com>
2025-07-25 16:08:15 +0500

    update

--- docs/update-readme-table.py
+++ docs/update-readme-table.py
@@ -60,7 +60,7 @@ for nb in data["notebooks"]:
         else ""
     )
     discussion_badge = (
-        f"[![Read Discussion](https://img.shields.io/badge/Documentation-yellow?logo=ultralytics)]({nb['discussion']})"
+        f"[![Read Discussion](https://img.shields.io/badge/Documentation-E1FF25?logo=ultralytics)]({nb['discussion']})"
         if nb.get("discussion")
         else ""
     )
