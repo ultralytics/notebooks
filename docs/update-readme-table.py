177c90cb22ba2aebca7e08c7b13c3928de6df643
Muhammad Rizwan Munawar <muhammadrizwanmunawar123@gmail.com>
2025-07-26 12:58:05 +0500

    remove centering comment

--- docs/update-readme-table.py
+++ docs/update-readme-table.py
@@ -28,10 +28,7 @@ for nb in data["notebooks"]:
     if nb.get("file"):
         title_link = f'<a href="{nb["file"]}">{nb["title"]}</a>'
     else:
-        if nb["file"] == "examples/tutorial.ipynb":
-            github_url = f"https://github.com/ultralytics/ultralytics/blob/main/{nb['file']}"
-        else:
-            github_url = f"https://github.com/ultralytics/notebooks/blob/main/{nb['file']}"
+        github_url = f"https://github.com/ultralytics/notebooks/blob/main/{nb['file']}"
         title_link = f"[{nb['title']}]({github_url})"
 
     # Generate all badges
